ad835cf2402194121e2383da077a54227bc16a1c
Ye Myo Aung <yemyo994@gmail.com>
2023-12-14 21:56:30 +0630

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mini_project_one/views/screens/detail_screen.dart';
 import 'package:mini_project_one/views/screens/home_screen.dart';
+import 'package:mini_project_one/views/screens/not_found_screen.dart';
+import 'package:mini_project_one/views/screens/photo_view_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MiniSocailApp());
 }
 
@@ -16,13 +19,28 @@ class MiniSocailApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color.fromRGBO(255, 255, 255, 0.8),
       ),
       onGenerateRoute: (settings) {
+        print(settings.name);
         switch (settings.name) {
+          case "/":
+            return MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+              settings: settings,
+            );
           case "/details":
             return MaterialPageRoute(
-                builder: (context) => const DetailScreen());
-
+              builder: (context) => const DetailScreen(),
+              settings: settings,
+            );
+          case "/view":
+            return MaterialPageRoute(
+              builder: (_) => PhotoViewScreen(),
+              settings: settings,
+            );
           default:
-            return MaterialPageRoute(builder: (context) => const HomeScreen());
+            return MaterialPageRoute(
+              builder: (_) => NotFoundScreen(),
+              settings: settings,
+            );
         }
       },
     );
