1cb4f26edc97ad0f5349c49b894c2f8ddaf81f77
Ye Myo Aung <yemyo994@gmail.com>
2023-12-14 09:25:15 +0630

    added post model

---
+++ lib/models/post.dart
@@ -0,0 +1,27 @@
+import 'package:mini_project_one/models/comment.dart';
+import 'package:mini_project_one/models/user.dart';
+
+class PostModel {
+  final int userId, id;
+  final String title, body;
+
+  final List<CommentModel> comments;
+
+  late UserModel user;
+
+  PostModel({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  }) : comments = [];
+
+  factory PostModel.fromJsObject(dynamic jsObject) {
+    return PostModel(
+      id: int.parse(jsObject['id'].toString()),
+      userId: int.parse(jsObject['userId'].toString()),
+      title: jsObject['title'],
+      body: jsObject['body'],
+    );
+  }
+}
