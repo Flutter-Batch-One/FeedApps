31e92620ddf5753338383069274cecd0979e2a23
Ye Myo Aung <yemyo994@gmail.com>
2023-12-14 09:24:55 +0630

    added comment model

---
+++ lib/models/comment.dart
@@ -0,0 +1,22 @@
+class CommentModel {
+  final int postId, id;
+  final String name, email, body;
+
+  CommentModel({
+    required this.postId,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.body,
+  });
+
+  factory CommentModel.fromJsObject(dynamic jsObject) {
+    return CommentModel(
+      id: int.parse(jsObject['id'].toString()),
+      postId: int.parse(jsObject['postId'].toString()),
+      name: jsObject['name'],
+      email: jsObject['email'],
+      body: jsObject['body'],
+    );
+  }
+}
